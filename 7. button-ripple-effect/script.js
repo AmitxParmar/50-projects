88d5934a537bfd48e5305ee1c062c11371b694ed
AmitxParmar <amitparmar901@gmail.com>
2023-02-14 20:53:28 +0530

    refactored folder titles

---
+++ 7. button-ripple-effect/script.js
@@ -0,0 +1,9 @@
+const buttons = document.querySelectorAll('.ripple')
+buttons.forEach(button => {
+    button.addEventListener('click', (e) => {
+        const x = e.pageX
+        const y = e.pageY
+
+        const buttonTop = <e className="target offsetTop">const buttonLeft</e>
+    })
+})
