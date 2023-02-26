2e3a822d01f0f02f16af2598f5749f4dd7d9819f
AmitxParmar <amitparmar901@gmail.com>
2023-02-26 23:24:26 +0530

    minor changes

--- 14. drawing-app/script.js
+++ 14. drawing-app/script.js
@@ -1,7 +1,7 @@
 const canvas = document.getElementById('canvas');
 const increaseBtn = document.getElementById('increase');
 const decreaseBtn = document.getElementById('decrease');
-const sizeEl = document.getElementById('size');
+const sizeEL = document.getElementById('size');
 const colorEl = document.getElementById('color');
 const clearEl = document.getElementById('clear');
 
